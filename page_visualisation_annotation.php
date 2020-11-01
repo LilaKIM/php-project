7c3ec148e4677d74a98ca6b11f72db50d1082090
LilaKIM <65628741+LilaKIM@users.noreply.github.com>
2020-11-02 03:51:01 +0900

    visu mode admin

--- page_visualisation_annotation.php
+++ page_visualisation_annotation.php
@@ -21,11 +21,18 @@ session_start();
     </head>
     <body>
         <?php
-            $id_utilisateur=$_SESSION['id'];
+            $id_utilisateur=$_SESSION['id_utilisateur'];
             echo "<p id='user'>".strtoupper($_SESSION['nom'])." ".ucfirst($_SESSION['prenom'])." (".$id_utilisateur.")</p>";
-
             # Menu automatique vers page d'accueil et visualisation_accord
             menu_auto("accueil, visualisation_accord");
+
+            # Test pour voir si l'utilisateur est admin 
+            # Si admin, on pourrait choisir l'id des autres pour regarder leurs annotations
+            $bdd=new PDO('mysql:host=localhost:8889;dbname=php-projet','root','root');
+            $statut = $bdd->query('SELECT statut FROM utilisateurs WHERE id LIKE '.$id_utilisateur);
+            while($data = $statut -> fetch()){$statut_utilisateur = $data['statut'];}
+            if ($statut_utilisateur == 'admin'){echo "admin";}else{echo $statut_utilisateur;}
+            $statut->closeCursor();
 
             #boutons permettant de selectionner un des critères (portion/taille_text/descriptieurs)
             echo "
@@ -34,28 +41,26 @@ session_start();
                 <INPUT type=\"submit\" name=\"taille_text\" value=\"Taille du text\">
                 <INPUT type=\"submit\" name=\"descripteurs\" value=\"Descripteurs\" >
             </form>";
-
+            
             # si bouton 'proportion' cliqué : on affiche une table contenant des registres avec de différentes proportions à choisir
             # après bouton 'rechercher', on n'a plus de cette table de choix... à modifier
             if(isset($_POST['proportion'])){
-                proportion($id_utilisateur);
+                proportion($id_utilisateur, $statut_utilisateur, $bdd);
             }
 
-            # si bouton 'taille text' cliqué : ?????
+            # si bouton 'taille text' cliqué : une table des extraits de taille indiquée s'affiche (id n'intervient pas)
             elseif(isset($_POST['taille_text'])){
-                taille_txt();
+                taille_txt($bdd);
             }
 
             # si bouton 'descripteurs' cliqué : on a une table des descripteurs à choix multiples
             elseif(isset($_POST['descripteurs'])){
-                descripteurs($id_utilisateur);
+                descripteurs($id_utilisateur, $statut_utilisateur, $bdd);
             }
         ?>
         <?php
-            function proportion($id_utilisateur){
+            function proportion($id_utilisateur, $statut, $bdd){
                 $registres=array("soutenu","courant","familier","poubelle");
-                $bdd=new PDO('mysql:host=localhost:8889;dbname=php-projet','root','root');
-
                 ##Pourquoi ça ne fonctionne pas quand post ?
                 echo "<form method=\"get\"><table>";
                 foreach ($registres as $value){
@@ -64,12 +69,28 @@ session_start();
                     <INPUT type=\"radio\" name=\"registre_proportion[$value]\" value=\"1\"> 25%
                     <INPUT type=\"radio\" name=\"registre_proportion[$value]\" value=\"2\"> 50%
                     <INPUT type=\"radio\" name=\"registre_proportion[$value]\" value=\"3\"> 75%
-                    <INPUT type=\"radio\" name=\"registre_proportion[$value]\" value=\"4\"> 100%</td></tr>";
+                    <INPUT type=\"radio\" name=\"registre_proportion[$value]\" value=\"4\"> 100%</td></tr><tr>";
                 }
-                echo "</table><br/><INPUT type=\"submit\" name=\"research_proportion\" value=\"Rechercher\"></form>";
-
-                ###???????????????????????????????????
+                # Préparer la liste des annotateurs si admin -> permet d'avoir un menu déroulant
+                if ($statut == 'admin'){
+                    $users_id=array();
+                    $users = $bdd->query('SELECT * FROM utilisateurs WHERE statut LIKE "annotateur"') or die(print_r($bdd->errorInfo()));;
+                    while ($data = $users->fetch()){$users_id[$data['id']] = strtoupper($data['nom'])." ".ucfirst($data['prenom'])." (".$data['id'].")";}
+                    // print_r($users_id);
+                    $users -> closeCursor();
+                    echo "<td><SELECT name=\"user_id\">";  
+                    foreach ($users_id as $key => $value){
+                        echo "<OPTION value=".$key.">".$value."</OPTION>";
+                    }
+                    echo "</SELECT></td>";
+                }
+                
+                echo "<td><INPUT type=\"submit\" name=\"research_proportion\" value=\"Rechercher\"></td></tr></table></form>";
+                ###??????????????????????????????????? pourquoi post ne marche pas à la place de get
                 if (isset($_GET['research_proportion'])){
+                    if ($_GET['user_id']){
+                        $id_utilisateur = $_GET['user_id'];
+                    }
                     $reg = $_GET['registre_proportion'];
                     // print_r($reg);
                     foreach ($reg as $value){
@@ -87,24 +108,32 @@ session_start();
                         while ($data = $annotation->fetch()){
                             array_push($id_extrait, $data['id_extrait']);
                         }
-                        // print_r($id_extrait);
-                        echo "<table>";
+                        $annotation -> closeCursor();
+
+                        $user_infos = $bdd -> query('SELECT * FROM utilisateurs WHERE id LIKE '.$id_utilisateur);
+                        while ($data = $user_infos->fetch()){echo "<table><tr><td><b>".$data['id']."</b></td><td><b>".strtoupper($data['nom'])." ".ucfirst($data['prenom'])."</b></td></tr>";}
+                        $user_infos -> closeCursor();
+
                         for ($i=0;$i<count($id_extrait);$i++){
                             $extrait = $bdd->query('SELECT * FROM corpus WHERE id_extrait LIKE '.$id_extrait[$i]);
                             while ($data = $extrait->fetch()){
                                 echo "<tr><td>".$data['id_extrait']."</td><td>".$data['extrait']."</td></tr>";
                             }
+                            $extrait ->closeCursor();
                         }
                         echo "</table>";
                     }
                 }
+                
+                $annotation->closeCursor();
+                $extrait->closeCursor();
+
             }
 
 
-            function taille_txt(){
-                $bdd=new PDO('mysql:host=localhost:8889;dbname=php-projet','root','root');
+            function taille_txt($bdd){
                 $menu_deroulant = array("Chercher les textes dont le nombre de caractères est", "inférieur à", "supérieur à", "entre");
-                echo "<form method=\"get\"><table><td><SELECT name=\"choix\">";
+                echo "<form method=\"get\"><table><tr><td><SELECT name=\"choix\">";
                 for ($i=1;$i<count($menu_deroulant);$i++){
                     if ($i!=count($menu_deroulant)-1){
                         echo "<OPTION value=".$menu_deroulant[$i].">".$menu_deroulant[$i]."</OPTION>";
@@ -157,6 +186,7 @@ session_start();
                         array_push($titre, $length);
                         // print_r($id_extrait);
                     }
+                    $annotation->closeCursor();
                     
                     echo "<table><tr><td>".$titre[0]."</td><td>".$titre[1]."</td></tr>";
                     for ($i=0;$i<count($id_extrait);$i++){
@@ -164,6 +194,7 @@ session_start();
                         while ($data = $extrait->fetch()){
                             echo "<tr><td>".$data['id_extrait']."</td><td>".$data['extrait']."</td></tr>";
                         }
+                        $extrait->closeCursor();
                     }
                     echo "</table>";
 
@@ -175,17 +206,31 @@ session_start();
             }
 
 
-            function descripteurs($id_utilisateur){
-                $bdd=new PDO('mysql:host=localhost:8889;dbname=php-projet','root','root');
+            function descripteurs($id_utilisateur, $statut, $bdd){
                 $descripteurs=$bdd->query('SELECT id_descripteur, descripteur FROM descripteurs');
                 echo "<form method=\"post\"><table>";
                 while ($descrips=$descripteurs->fetch()) {
-                    echo "<tr><td><INPUT type=\"checkbox\" name=\"descripteurs[]\" value=".$descrips['id_descripteur'].">".$descrips['descripteur']."</td></tr>";
+                    echo "<tr><td><INPUT type=\"checkbox\" name=\"descripteurs[]\" value=".$descrips['id_descripteur'].">".$descrips['descripteur']."</td></tr><tr>";
                 }
+                $descripteurs->closeCursor();
 
-                echo "</table><br/><INPUT type=\"submit\" name=\"research_desc\" value=\"Rechercher\"></form>";
-
+                if ($statut == 'admin'){
+                    $users_id=array();
+                    $users = $bdd->query('SELECT * FROM utilisateurs WHERE statut LIKE "annotateur"') or die(print_r($bdd->errorInfo()));;
+                    while ($data = $users->fetch()){$users_id[$data['id']] = strtoupper($data['nom'])." ".ucfirst($data['prenom'])." (".$data['id'].")";}
+                    // print_r($users_id);
+                    $users -> closeCursor();
+                    echo "<td><SELECT name=\"user_id\">";  
+                    foreach ($users_id as $key => $value){
+                        echo "<OPTION value=".$key.">".$value."</OPTION>";
+                    }
+                    echo "</SELECT></td>";
+                }
+                echo "<td><INPUT type=\"submit\" name=\"research_desc\" value=\"Rechercher\"></td></tr></table></form>";
                 if (isset($_POST['research_desc'])){
+                    if ($_POST['user_id']){
+                        $id_utilisateur = $_POST['user_id'];
+                    }
                     $id_extrait =array();
                     $desc = $_POST['descripteurs'];
                     $class_desc= array("des_c", "des_f", "des_s", "des_p");
@@ -199,12 +244,18 @@ session_start();
                             }
                         }
                     }
-                    echo "<table>";
+                    $annotation->closeCursor();
+
+                    $user_infos = $bdd -> query('SELECT * FROM utilisateurs WHERE id LIKE '.$id_utilisateur);
+                        while ($data = $user_infos->fetch()){echo "<table><tr><td><b>".$data['id']."</b></td><td><b>".strtoupper($data['nom'])." ".ucfirst($data['prenom'])."</b></td></tr>";}
+                        $user_infos -> closeCursor();
+
                     for ($i=0;$i<count($id_extrait);$i++){
                         $extrait = $bdd->query('SELECT * FROM corpus WHERE id_extrait LIKE '.$id_extrait[$i]);
                         while ($data = $extrait->fetch()){
                             echo "<tr><td>".$data['id_extrait']."</td><td>".$data['extrait']."</td></tr>";
                         }
+                        $extrait->closeCursor();
                     }
                     echo "</table>";
 
